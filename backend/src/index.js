fb040ceedff86710b07c61d615e2c6ed69670d40
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-29 18:26:15 -0300

    feat(auth): implement user registration with Express, JWT, and PassportJS

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { Mongo } from './database/mongo.js';
 import {config} from 'dotenv';
+import authRouter from './auth/auth.js';
 
 config()
 
@@ -27,6 +28,8 @@ async function main(){
             body: 'Welcome to Gourmertize!'
         })
     })
+
+    app.use('/auth', authRouter)
 
     app.listen(port, () => {
         console.log(`Server is running at http://${hostname}:${port}`);
